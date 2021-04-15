f936167b1843c43b20f44c6b1ef89c7aaee67c8d
Conner Vieira <connervieira@tutanota.com>
2021-04-15 08:52:47 -0400

    Added more comments

--- import_databases.php
+++ import_databases.php
@@ -1,10 +1,11 @@
 <?php
 
+// Define function to check if a database exists and load it if it does.
 function load_database($database_to_load) {
     if (file_exists($database_to_load)) {  // Check to see if the database file exists.
         return unserialize(file_get_contents($database_to_load)); // Load the database.
     } else {
-        echo "<p style='color:red;'>Error: Could not load database '" . $database_to_load . "'. The database file was not found.</p>";
+        echo "<p style='color:red;'>Error: Could not load database '" . $database_to_load . "'. The database file was not found.</p>"; // Alert that the database couldn't be opened.
     }
 }
 
