3b248c291f72f04aefc35a43a1f88e32e5379b69
Conner Vieira <connervieira@tutanota.com>
2021-04-15 08:58:28 -0400

    Added better comments

--- employeeremove.php
+++ employeeremove.php
@@ -1,19 +1,25 @@
-<?php
-include("./import_databases.php");
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Marathon - Remove Employee</title>
+    </head>
+    <body>
+        <?php
+        include("./import_databases.php");
 
-$id_to_delete = $_GET["id"];
-$confirmation = $_GET["confirmation"];
+        $id_to_delete = $_GET["id"];
+        $confirmation = $_GET["confirmation"];
 
-if ($confirmation !== "true") {
-    echo "<p>Confirm deletion</p>";
-    echo "<p><a href='employeeremove.php?id=" . $id_to_delete . "&confirmation=true'>Confirm</a></p>";
-    echo "<p><a href='employees.php'>Cancel</a></p>";
-    exit();
-} else {
-    unset($employee_database[$id_to_delete]);
-    file_put_contents('./databases/employeedatabase.txt', serialize($employee_database)); // Write array changes to disk
-    echo "<p>Employee deleted</p>";
-    echo "<p><a href='employees.php'>Back</a></p>";
-}
-
-?>
+        if ($confirmation !== "true") { // If the user hasn't yet pressed 'Confirm', the display the 'Confirm' button.
+            echo "<p>Confirm deletion</p>";
+            echo "<p><a href='employeeremove.php?id=" . $id_to_delete . "&confirmation=true'>Confirm</a></p>";
+            echo "<p><a href='employees.php'>Cancel</a></p>";
+        } else { // Otherwise, delete the employee.
+            unset($employee_database[$id_to_delete]); // Remove the employee from the database
+            file_put_contents('./databases/employeedatabase.txt', serialize($employee_database)); // Write array changes to disk
+            echo "<p>Employee deleted</p>";
+            echo "<p><a href='employees.php'>Back</a></p>";
+        }
+        ?>
+    </body>
+</html>
