4856fdcee9ac738aeb60ada58a3be6006df15574
Conner Vieira <connervieira@tutanota.com>
2022-11-15 14:03:11 -0500

    Databases are now created by Marathon itself

--- import_databases.php
+++ import_databases.php
@@ -3,14 +3,16 @@
 // Define function to check if a database exists and load it if it does.
 function load_database($database_directory, $database_to_load) {
     if (is_writable($database_directory)) { // Check to make sure the database directory is writable.
-        if (is_writable($database_directory . "/" . $database_to_load)) { // Check to make sure the database itself is writable.
-            if (file_exists($database_directory . "/" . $database_to_load)) {  // Check to see if the database file exists.
-                return unserialize(file_get_contents($database_directory . "/" . $database_to_load)); // Load the database.
-            } else {
-                echo "<p style='color:red;'>Error: Could not load database '" . $database_to_load . "'. The database file was not found.</p>"; // Alert that the database couldn't be opened.
+        if (!file_exists($database_directory . "/" . $database_to_load)) {  // Check to see if the database file doesn't exists.
+            file_put_contents($database_directory . "/" . $database_to_load, "a:0:{}"); // Save a blank placeholder to the database file.
+        }
+        if (file_exists($database_directory . "/" . $database_to_load)) {  // Check to see if the database file exists. It should have been created in a previous step if not.
+            return unserialize(file_get_contents($database_directory . "/" . $database_to_load)); // Load the database.
+            if (is_writable($database_directory . "/" . $database_to_load)) { // Check to make sure the database itself is writable.
+                echo "<p style='color:red;'>Error: The database '" . $database_to_load . "' is not writable.</p>"; // Alert that the database is not writable.
             }
         } else {
-            echo "<p style='color:red;'>Error: Could not load database '" . $database_to_load . "'. The database file is not writable.</p>"; // Alert that the database couldn't be opened.
+            echo "<p style='color:red;'>Error: Could not load database '" . $database_to_load . "'. The database file was not found.</p>"; // Alert that the database couldn't be opened.
         }
     } else {
         echo "<p style='color:red;'>Error: Could not load database '" . $database_to_load . "'. The database directory is not writable.</p>"; // Alert that the database couldn't be opened.
